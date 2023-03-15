ab9cc2325bc85ba05a8b3e596a867cb1504504e1
King <francisking1st@gmail.com>
2023-03-15 14:53:07 +0100

    updated guest track

--- public/js/babcockmap2.js
+++ public/js/babcockmap2.js
@@ -1,10 +1,10 @@
-async function initMap(){
+function initMap(){
     var options = {
         zoom: 15,
         center: {lat: 6.8920793, lng:3.7181458}
     }
 
-    var map1 = await new google.maps.Map(document.querySelector('#map3'), options)
+    var map1 = new google.maps.Map(document.getElementById('mapp'), options)
 
 
     var marker = new google.maps.Marker({
@@ -13,14 +13,14 @@ async function initMap(){
     })
 }
 
-// function displayRegister(){
-//     const map = document.querySelector(".onmap")
-//     console.log("im on map");
-//     map.style.opacity = "1";
-// }
+function displayRegister(){
+    const map = document.querySelector(".onmap")
+    console.log("im on map");
+    map.style.opacity = "1";
+}
 
-// function removeRegister(){
-//     const map = document.querySelector(".onmap")
-//     console.log("remove map");
-//     map.style.opacity = "0"
-// }
+function removeRegister(){
+    const map = document.querySelector(".onmap")
+    console.log("remove map");
+    map.style.opacity = "0"
+}
